008abe08242f6c578a419a3b8f5ea36d3832051b
zbadiru <zbadiru@live.co.uk>
2020-06-03 12:40:45 +0100

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ function moveDodgerLeft() {
         dodger.style.left = `${left - 1}px`;
     }
 }
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+})
+
 function moveDodgerRight() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
@@ -15,3 +21,8 @@ function moveDodgerRight() {
         dodger.style.left = `${left + 1}px`;
     }
 }
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
